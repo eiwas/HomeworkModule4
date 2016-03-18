b5a2d37c167388303385ef299da86c4cbb5f5fdf
eiwas <m.khomulenko@gmail.com>
2016-03-19 00:28:05 +0300

    Initial commit

---
+++ src/com/goitonline/module5/SortingLogic.java
@@ -0,0 +1,34 @@
+package com.goitonline.module5;
+
+import java.util.Arrays;
+
+public class SortingLogic {
+    public int[] arraySorter(int[] myArray2) {
+        boolean changeHappened = true;
+        while (changeHappened) {
+            changeHappened = false;
+            for (int i = 0; i < myArray2.length - 1; i++) {
+                if (myArray2[i] > myArray2[i + 1]) {
+                    int tempVar = myArray2[i];
+                    myArray2[i] = myArray2[i + 1];
+                    myArray2[i + 1] = tempVar;
+                    changeHappened = true;
+                }
+            }
+        }
+        return myArray2;
+
+    }
+    public int getMinValue (int[] myArray) {
+        int min = 0;
+        Arrays.sort(myArray);
+        min = myArray[0];
+        return min;
+    }
+    public int getMaxValue (int[] myArray) {
+        int max = 0;
+        Arrays.sort(myArray);
+        max = myArray[myArray.length-1];
+        return max;
+    }
+}
